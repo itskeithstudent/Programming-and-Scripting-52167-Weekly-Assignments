a4dfe55d97bdd7f09293478c58fc3a2b05c1075a
itskeith <itskeithryan@gmail.com>
2020-01-22 21:20:52 +0000

    initial commit, first python assignment from week 2 'Week 2 Assignment.py'

---
+++ Week 2 Assignment.py
@@ -0,0 +1,9 @@
+#Simple program to take user input for Weight (in kg) and Height (in cm)
+#Then we caclulate the user height in meters squared, finally we calculate the BMI
+print('Enter Weight: ')
+user_weight_kg = int(input())
+print('Enter Height: ')
+user_height_cm = int(input())
+user_heigh_msquared = (user_height_cm/100)**2
+user_bmi = round((user_weight_kg/user_heigh_msquared),2)
+print("BMI: ", str(user_bmi))
