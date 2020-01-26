0bbb46eb81ade73c74481f4dc6a3f7078a4fb5f8
itskeithstudent <G00387816@gmit.ie>
2020-01-26 19:14:44 +0000

    condense print and input statements for user_weight_kg and user_height_cm

--- Week 2 Assignment.py
+++ Week 2 Assignment.py
@@ -1,9 +1,7 @@
 #Simple program to take user input for Weight (in kg) and Height (in cm)
 #Then we caclulate the user height in meters squared, finally we calculate the BMI
-print('Enter Weight: ')
-user_weight_kg = int(input())
-print('Enter Height: ')
-user_height_cm = int(input())
+user_weight_kg = int(input('Enter Weight: '))
+user_height_cm = int(input('Enter Height: '))
 user_heigh_msquared = (user_height_cm/100)**2
 user_bmi = round((user_weight_kg/user_heigh_msquared),2)
 print("BMI: ", str(user_bmi))
