ccdf98d64192d34355f20127e0b771623eca26db
itskeithstudent <G00387816@gmit.ie>
2020-01-29 22:42:04 +0000

    'Week 3 Assingment'.py added for week 3 assignment

---
+++ Week 3 Assignment.py
@@ -0,0 +1,21 @@
+
+
+#Take input from user as string, output every second letter in reverse order
+user_input_str = str(input('Please enter a sentence: '))
+
+#take first char off start of string
+step_str = user_input_str[1:]
+#now get every second char starting with the first char
+step_str = step_str[::2]
+#reverse the order of chars
+reverse_step_str = step_str[::-1]
+print(reverse_step_str)
+
+#better solution
+step_str = user_input_str[1::2]
+reverse_step_str = step_str[::-1]
+print(reverse_step_str)
+
+#even better solution
+step_str = user_input_str[1::2][::-1]
+print(step_str)
