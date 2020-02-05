5ff9cd3aaef35f0174f792a293ebcfc14d2f78d4
itskeithstudent <G00387816@gmit.ie>
2020-02-05 23:04:35 +0000

    fix for not initializing concat_operation

--- Week 4 Assignment.py
+++ Week 4 Assignment.py
@@ -5,18 +5,16 @@ user_input = int(input("Please enter a happy (positive) number - "))
 #we multiply it by three and add 1
 def oddNum(odd_num):
     return (odd_num*3)+1
-
+concat_operation=''
 #if my user inputs 1 all we do is print 1
 if(user_input==1):
     print(user_input)
 #else we start to enter a while loop which remains true until the user_input is no longer > 1
 else:
     while(user_input>=1):
-        print(user_input)
         concat_operation = concat_operation + f' {user_input}'
         user_input = user_input//2
         if user_input % 2 and user_input != 1:
-            print(user_input)
             concat_operation = concat_operation + f' {user_input}'
             user_input = oddNum(user_input)
 
