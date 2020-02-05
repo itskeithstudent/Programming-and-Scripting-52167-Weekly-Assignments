b1b8a23d276666478191773482201bada1b42f31
itskeithstudent <G00387816@gmit.ie>
2020-02-05 22:52:36 +0000

    prints one concatenated string of all numbers

--- Week 4 Assignment.py
+++ Week 4 Assignment.py
@@ -1,22 +1,26 @@
 #program takes integer input from user
 user_input = int(input("Please enter a happy (positive) number - "))
 
+#definition to do odd number calculation, when we get an odd number after dividing some number by 2
+#we multiply it by three and add 1
 def oddNum(odd_num):
     return (odd_num*3)+1
 
 #if my user inputs 1 all we do is print 1
 if(user_input==1):
-    print(1)
+    print(user_input)
 #else we start to enter a while loop which remains true until the user_input is no longer > 1
 else:
     while(user_input>=1):
         print(user_input)
+        concat_operation = concat_operation + f' {user_input}'
         user_input = user_input//2
         if user_input % 2 and user_input != 1:
             print(user_input)
+            concat_operation = concat_operation + f' {user_input}'
             user_input = oddNum(user_input)
 
-
+print(concat_operation)
 
 
 
