2879536d98d2b489c62c2806017e02588d09a84a
itskeithstudent <G00387816@gmit.ie>
2020-02-12 19:50:59 +0000

    Accounted for lowercase or uppercase in user supplied string

--- Week 5 Assignment.py
+++ Week 5 Assignment.py
@@ -1,13 +1,14 @@
 #A program that outputs whether or not today is a weekday.
 
-weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
-weekend = ["Saturday", "Sunday"]
+#these two lists are lowercase, in case we get variance in use of case in user supplied string e.g. Monday/TUESdaY/etc.
+weekdays = ["monday","tuesday","wednesday","thursday","friday"]
+weekend = ["saturday", "sunday"]
 #take in day of the week from user
 curr_day=str(input("Enter what day of the week it is and I'll tell you whether it's the weekend or not. "))
 
-if curr_day in weekdays:
+if curr_day.lower() in weekdays:
     print("My sincere condolences, it is a week day, get back to work...")
-elif curr_day in weekend:
+elif curr_day.lower() in weekend:
     print("Congratulations it is the weekend, it will be a week day soon...")
 else:
     print("Sorry you didn't enter a valid day")
