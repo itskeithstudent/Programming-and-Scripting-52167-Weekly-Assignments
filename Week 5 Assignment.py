7de176c4074b2bcc4e80fbf43f324053d4922511
itskeithstudent <G00387816@gmit.ie>
2020-02-19 21:43:51 +0000

    Added solution using a dict, fixed issue with case on today var

--- Week 5 Assignment.py
+++ Week 5 Assignment.py
@@ -2,6 +2,11 @@
 #   Week 5 Assignment
 #   A program that outputs whether or not today is a weekday.
 
+from datetime import datetime
+
+#########################
+### Original solution ###
+#########################
 #these two lists are lowercase, in case we get variance in use of case in user supplied string e.g. Monday/TUESdaY/etc.
 weekdays = ("monday","tuesday","wednesday","thursday","friday")
 weekend = ("saturday", "sunday")
@@ -18,14 +23,32 @@ elif curr_day.lower() in weekend:
 else:
     print("Sorry you didn't enter a valid day")
 
-########################
-###Alternate solution###
-########################
+#####################
+### Best solution ###
+#####################
 #add logic to get current day from current date and time.
-from datetime import datetime
 todays_datetime = datetime.today() #get todays date and time from calling datetimes today function
 today = todays_datetime.strftime('%A') #get today using strftime function of datetime.datetime object
-if today in weekdays: #same logic as above, but we don't need a final else block in case user enters wrong date as we don't have any user entry for here
+if str.lower(today) in weekdays: #same logic as above, but we don't need a final else block in case user enters wrong date as we don't have any user entry for here
     print("My sincere condolences, it is a week day, get back to work...")
 else:
     print("Congratulations it is the weekend, it will be a week day soon...")
+
+##########################
+### Alternate solution ###
+##########################
+#Neat way of doing it with dictionary
+#probably too much typing though, but is very readable
+congrats_str = "Congratulations it is the weekend, it will be a week day soon..."
+condolences_str = "My sincere condolences, it is a week day, get back to work..."
+days_of_the_week_dict = {
+    "Monday":condolences_str,
+    "Tuesday":condolences_str,
+    "Wednesday":condolences_str,
+    "Thursday":condolences_str,
+    "Friday":condolences_str,
+    "Saturday":congrats_str,
+    "Sunday":congrats_str
+}
+
+print(days_of_the_week_dict[today])
