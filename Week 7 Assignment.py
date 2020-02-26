ac0056a27c88ddbafb9743c182cb4c8d2897fb80
itskeithstudent <G00387816@gmit.ie>
2020-02-26 19:20:03 +0000

    Skeleton of week 7 assignment added - takes in user input and prints back again

---
+++ Week 7 Assignment.py
@@ -0,0 +1,12 @@
+#   Keith Ryan
+#   Week 7 Assignment
+# function to read in file and count number of letter 'e'
+# Write a program that reads in a text file and outputs the number of e's it contains. The program should take the filename from an argument on the command line.
+
+# this function takes an input from user that should point to folder location containing a .txt file
+def countLetterE(path):
+
+    print(path)
+
+user_input = input("Enter the path to a file you want to count the times the letter e occurs: ")
+countLetterE(user_input)
