313cc91c57688d2476a447cd406c9d544d12a2b1
itskeithstudent <G00387816@gmit.ie>
2020-02-26 19:48:30 +0000

    Corrected to take users input from command line

--- Week 7 Assignment.py
+++ Week 7 Assignment.py
@@ -3,6 +3,7 @@
 # function to read in file and count number of letter 'e'
 # Write a program that reads in a text file and outputs the number of e's it contains. The program should take the filename from an argument on the command line.
 
+import sys
 # this function takes an input from user that should point to folder location containing a .txt file
 def countLetterE(path):
     count=0 #count is initialised to 0 on calling function as we have yet to count any 'e's
@@ -13,5 +14,6 @@ def countLetterE(path):
             count+=1
     print(count) #once we've parsed through every char in textFileStr print the count
 
-user_input = input("Enter the path to a file you want to count the times the letter e occurs: ")
-countLetterE(user_input)
+#user_input = input("Enter the path to a file you want to count the times the letter e occurs: ")
+commandLineInput = sys.argv[1]
+countLetterE(commandLineInput)
