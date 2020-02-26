f027c7f9b951148c59d3a1e516fb2ca34607bbe3
itskeithstudent <G00387816@gmit.ie>
2020-02-26 20:12:55 +0000

    Add try except finally block to countLetterE function to ensure file always gets closed if opened

--- Week 7 Assignment.py
+++ Week 7 Assignment.py
@@ -7,13 +7,20 @@ import sys
 # this function takes an input from user that should point to folder location containing a .txt file
 def countLetterE(path):
     count=0 #count is initialised to 0 on calling function as we have yet to count any 'e's
+    #add try finally block to ensure file always get's closed
     textFile = open(path) #open user supplied path
-    textFile.close()
-    textFileStr = textFile.read() #take the file we've opened, read it and store in string variable textFileStr
-    for char in textFileStr: #for each character in textFileStr check if 'e'
-        if char.lower() == 'e': #consider case where e is upper or lower case
-            count+=1
-    print(count) #once we've parsed through every char in textFileStr print the count
+    try:
+        textFileStr = textFile.read() #take the file we've opened, read it and store in string variable textFileStr
+        #close back down the opened text file
+        for char in textFileStr: #for each character in textFileStr check if 'e'
+            if char.lower() == 'e': #consider case where e is upper or lower case
+                count+=1
+        print(count) #once we've parsed through every char in textFileStr print the count
+    except:
+        print("Encountered some error with opened file") #incase user points us to a file we can't open or read print this
+    finally:
+        print("Finally")
+        textFile.close() #regardless of what happens above, close the file after opening it
 
 #try except blocks first trys to get commandLineInput
 try:
