52b0c16299effe27855e30340060b0458988884d
itskeithstudent <G00387816@gmit.ie>
2020-03-11 21:43:37 +0000

    Add requirements.txt for installing required packages

--- Week 4 Assignment.py
+++ Week 4 Assignment.py
@@ -1,7 +1,7 @@
 #   Keith Ryan
 #   Week 4 Assignment
 #   program takes integer input from user
-#   It asks the user to input any positive integer and outputs the successive values of the following calculation. 
+#   It asks the user to input any positive integer and outputs the successive values of the following calculation.
 #   At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
 #   Have the program end if the current value is one.
 user_input = int(input("Please enter a happy (positive) number - "))
