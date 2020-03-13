9b8d0737fc55bb118fd548618e288bb64d5a54a5
itskeithstudent <G00387816@gmit.ie>
2020-03-13 19:12:44 +0000

    first commit for week 8 assignment, add basic functions for f,g and h mathematical functions and simple print statement to show their values growing exponentially

---
+++ Week 8 Assignment.py
@@ -0,0 +1,18 @@
+#   Keith Ryan
+#   Week 8 Assignment
+#   plot three mathematical functions using matplot.pyplot: f(x), g(x) and h(x)
+#   where f(x)=x, g(x)=x**2 and h(x)==x**3
+
+def f(x):
+    return(x)
+
+def g(x):
+    return(x**2)
+
+def h(x):
+    return(x**3)
+
+for i in range(4):
+    print(f(i))
+    print(g(i))
+    print(h(i))
