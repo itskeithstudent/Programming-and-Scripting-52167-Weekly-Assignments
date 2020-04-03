b00e8552178cf32d7d797f63df2d08a999b7dcb3
itskeithstudent <G00387816@gmit.ie>
2020-04-03 17:24:48 +0100

    update comments and replaced str concatenation in print with f string

--- Week 2 Assignment.py
+++ Week 2 Assignment.py
@@ -4,6 +4,6 @@
 #Then we caclulate the user height in meters squared, finally we calculate the BMI
 user_weight_kg = int(input('Enter Weight (in Kg): ')) #Convert user input to int, assumes user will enter a int value
 user_height_cm = int(input('Enter Height (in cm): '))
-user_heigh_msquared = (user_height_cm/100)**2
+user_heigh_msquared = (user_height_cm/100)**2 # ** operater does power, **2 is squared, **3 is cubed, etc. see: https://docs.python.org/3.4/library/operator.html
 user_bmi = round((user_weight_kg/user_heigh_msquared),2) #Round to two decimal palces the users bmi
-print("BMI: ", str(user_bmi))
+print(f"BMI: {str(user_bmi)}")
